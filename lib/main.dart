d4019bf95b39057a7b9694c5de18de7400aa17d0
Léo Kiszewski <kiszewski1999@gmail.com>
2020-10-05 16:05:04 -0300

    favorite products view creation

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:provider/provider.dart';
 import 'package:shopApp/models/cart_model.dart';
 import 'package:shopApp/models/favorite_model.dart';
 import 'package:shopApp/models/product_model.dart';
+import 'package:shopApp/models/products_model.dart';
 import 'package:shopApp/pages/home_page/home_page.dart';
 import 'package:shopApp/pages/product_details_page/product_details_page.dart';
 
@@ -11,33 +12,11 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  final List<ProductModel> products = [
-    ProductModel(
-        'Black T-shirt',
-        'https://lp2.hm.com/hmgoepprod?set=quality[79],source[/f5/c4/f5c4939114fcc731acfada4ebb68f1da42cad909.jpg],origin[dam],category[men_tshirtstanks_shortsleeve],type[DESCRIPTIVESTILLLIFE],res[m],hmver[1]&call=url[file:/product/main]',
-        89,
-        'Camiseta preta lisa'),
-    ProductModel(
-        'White T-shirt',
-        'https://cdn.shoplo.com/9107/products/th2048/abam/3740-1106-1346-14.png',
-        89,
-        'Camiseta branca lisa'),
-    ProductModel(
-        'Red T-shirt',
-        'https://cdn.awsli.com.br/600x450/44/44273/produto/29989858/b28e079baa.jpg',
-        89,
-        'Camiseta vermelha lisa'),
-    ProductModel(
-        'Tenis preto',
-        'https://artwalk.vteximg.com.br/arquivos/ids/218426-1000-1000/Tenis-Nike-Air-Max-97-Masculino-Multicolor.jpg?v=637091629305300000',
-        89,
-        'Tenis Nike preto'),
-  ];
-
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
+        ChangeNotifierProvider(create: (context) => ProductsModel()),
         ChangeNotifierProvider(create: (context) => CartModel()),
         ChangeNotifierProvider(create: (context) => FavoriteModel()),
       ],
@@ -49,7 +28,7 @@ class MyApp extends StatelessWidget {
           visualDensity: VisualDensity.adaptivePlatformDensity,
         ),
         routes: {
-          '/': (context) => HomePage('MyShop', products),
+          '/': (context) => HomePage('MyShop'),
           'product_details': (context) => ProductDetailsPage(),
         },
       ),
--- lib/pages/home_page/home_page.dart
+++ lib/pages/home_page/home_page.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shopApp/models/cart_model.dart';
-import 'package:shopApp/models/product_model.dart';
+import 'package:shopApp/models/favorite_model.dart';
+import 'package:shopApp/models/products_model.dart';
 import 'package:shopApp/pages/home_page/components/product_card_view.dart';
 import 'package:shopApp/size_config.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   final String title;
-  final List<ProductModel> products;
 
   static const Map<String, IconData> drawerOptions = {
     'Shop': Icons.shopping_cart,
@@ -15,17 +15,44 @@ class HomePage extends StatelessWidget {
     'Manage Products': Icons.edit
   };
 
-  const HomePage(this.title, this.products);
+  HomePage(this.title);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  bool onlyFavorites = false;
+
+  _changeProductsList(bool value) {
+    setState(() {
+      onlyFavorites = value;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
+    final ProductsModel productsModel = Provider.of<ProductsModel>(context);
+    final FavoriteModel favoriteModel = Provider.of<FavoriteModel>(context);
     SizeConfig().init(context);
 
     return Scaffold(
       appBar: AppBar(
-        title: Text(title),
+        title: Text(widget.title),
         actions: [
-          IconButton(icon: Icon(Icons.more_vert), onPressed: () {}),
+          Center(
+            child: DropdownButton(
+              dropdownColor: Colors.white,
+              iconEnabledColor: Colors.white,
+              underline: SizedBox(),
+              icon: Icon(Icons.more_vert),
+              onChanged: _changeProductsList,
+              items: [
+                DropdownMenuItem(child: Text('Somente favoritos'), value: true),
+                DropdownMenuItem(child: Text('Mostrar todos'), value: false)
+              ],
+            ),
+          ),
           Center(
             child: Stack(
               children: [
@@ -71,11 +98,12 @@ class HomePage extends StatelessWidget {
               height: SizeConfig.blockSizeVertical * 80,
               width: double.maxFinite,
               child: ListView.builder(
-                  itemCount: drawerOptions.length,
+                  itemCount: HomePage.drawerOptions.length,
                   itemBuilder: (context, index) {
                     return ListTile(
-                      leading: Icon(drawerOptions.values.toList()[index]),
-                      title: Text(drawerOptions.keys.toList()[index]),
+                      leading:
+                          Icon(HomePage.drawerOptions.values.toList()[index]),
+                      title: Text(HomePage.drawerOptions.keys.toList()[index]),
                     );
                   }),
             ),
@@ -89,7 +117,9 @@ class HomePage extends StatelessWidget {
           padding: EdgeInsets.all(SizeConfig.blockSizeVertical * 1.5),
           crossAxisSpacing: SizeConfig.blockSizeVertical * 1.5,
           mainAxisSpacing: SizeConfig.blockSizeVertical * 1.5,
-          children: products.map((e) => ProductCardView(e)).toList(),
+          children: onlyFavorites
+              ? favoriteModel.favorites.map((e) => ProductCardView(e)).toList()
+              : productsModel.products.map((e) => ProductCardView(e)).toList(),
         ),
       ),
     );
