d0d903583b7ac2b4248fcc4987a34f4638d99a74
Léo Kiszewski <kiszewski1999@gmail.com>
2020-12-07 20:01:31 -0300

    added stream on home page

--- lib/pages/home_page/home_page.dart
+++ lib/pages/home_page/home_page.dart
@@ -20,6 +20,14 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   bool onlyFavorites = false;
 
+  Stream<List<ProductModel>> _productsStream;
+
+  @override
+  void initState() {
+    super.initState();
+    _productsStream = context.read<ProductsViewModel>().getProducts();
+  }
+
   _changeProductsList(bool value) {
     setState(() {
       onlyFavorites = value;
@@ -28,105 +36,115 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    final ProductsViewModel productsModel =
-        Provider.of<ProductsViewModel>(context);
     final FavoriteViewModel favoriteModel =
         Provider.of<FavoriteViewModel>(context);
 
-    final List<ProductModel> productsToShow = onlyFavorites
-        ? favoriteModel.favorites.toList()
-        : favoriteModel.favorites.toList();
-
     return Scaffold(
-      appBar: AppBar(
-        iconTheme: IconThemeData(color: Colors.black87),
-        title: Text(
-          widget.title,
-          style: TextStyle(color: Colors.black87),
-        ),
-        backgroundColor: Colors.white12,
-        elevation: 0,
-        actions: [
-          Center(
-            child: DropdownButton(
-              dropdownColor: Colors.white,
-              iconEnabledColor: Colors.black87,
-              underline: SizedBox(),
-              icon: Icon(Icons.more_vert),
-              onChanged: _changeProductsList,
-              items: [
-                DropdownMenuItem(child: Text('Somente favoritos'), value: true),
-                DropdownMenuItem(child: Text('Mostrar todos'), value: false)
-              ],
-            ),
+        appBar: AppBar(
+          iconTheme: IconThemeData(color: Colors.black87),
+          title: Text(
+            widget.title,
+            style: TextStyle(color: Colors.black87),
           ),
-          Center(
-            child: Stack(
-              children: [
-                IconButton(
-                  icon: Icon(Icons.shopping_cart),
-                  color: Colors.black87,
-                  onPressed: () => Navigator.pushNamed(context, 'cart'),
-                ),
-                Positioned(
-                  right: 10,
-                  top: 5,
-                  child: ClipRRect(
-                    child: Container(
-                      width: SizeConfig.blockSizeHorizontal * 3,
-                      height: SizeConfig.blockSizeHorizontal * 3,
-                      color: Theme.of(context).primaryColor,
-                      child: FittedBox(
-                        fit: BoxFit.scaleDown,
-                        child: Consumer<CartViewModel>(
-                          builder: (context, cart, child) {
-                            return Text(cart.qtdProducts.toString());
-                          },
+          backgroundColor: Colors.white12,
+          elevation: 0,
+          actions: [
+            Center(
+              child: DropdownButton(
+                dropdownColor: Colors.white,
+                iconEnabledColor: Colors.black87,
+                underline: SizedBox(),
+                icon: Icon(Icons.more_vert),
+                onChanged: _changeProductsList,
+                items: [
+                  DropdownMenuItem(
+                      child: Text('Somente favoritos'), value: true),
+                  DropdownMenuItem(child: Text('Mostrar todos'), value: false)
+                ],
+              ),
+            ),
+            Center(
+              child: Stack(
+                children: [
+                  IconButton(
+                    icon: Icon(Icons.shopping_cart),
+                    color: Colors.black87,
+                    onPressed: () => Navigator.pushNamed(context, 'cart'),
+                  ),
+                  Positioned(
+                    right: 10,
+                    top: 5,
+                    child: ClipRRect(
+                      child: Container(
+                        width: SizeConfig.blockSizeHorizontal * 3,
+                        height: SizeConfig.blockSizeHorizontal * 3,
+                        color: Theme.of(context).primaryColor,
+                        child: FittedBox(
+                          fit: BoxFit.scaleDown,
+                          child: Consumer<CartViewModel>(
+                            builder: (context, cart, child) {
+                              return Text(cart.qtdProducts.toString());
+                            },
+                          ),
                         ),
                       ),
+                      borderRadius: BorderRadius.circular(90),
                     ),
-                    borderRadius: BorderRadius.circular(90),
-                  ),
-                )
-              ],
-            ),
-          )
-        ],
-      ),
-      drawer: DrawerView(),
-      body: productsToShow.isNotEmpty
-          ? GridView.builder(
-              itemBuilder: (context, index) {
-                return ProductCardComponent(productsToShow[index]);
-              },
-              physics: BouncingScrollPhysics(),
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
-                childAspectRatio: 1.5,
-                crossAxisSpacing: SizeConfig.blockSizeVertical * 1.5,
-                mainAxisSpacing: SizeConfig.blockSizeVertical * 1.5,
-              ),
-              itemCount: productsToShow.length,
-              padding: EdgeInsets.all(SizeConfig.blockSizeVertical * 1.5),
-            )
-          : Container(
-              width: SizeConfig.blockSizeHorizontal * 100,
-              height: SizeConfig.blockSizeVertical * 100,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Image.asset('assets/shop.png'),
-                  onlyFavorites
-                      ? Text(
-                          'Lista de favoritos vazia',
-                          style: TextStyle(fontSize: 16),
-                        )
-                      : Text(
-                          'Lista de produtos vazia',
-                          style: TextStyle(fontSize: 16),
-                        ),
+                  )
                 ],
-              )),
-    );
+              ),
+            )
+          ],
+        ),
+        drawer: DrawerView(),
+        body: StreamBuilder<List<ProductModel>>(
+          stream: _productsStream,
+          builder: (context, snapshot) {
+            if (snapshot.hasError) {
+              return Text(snapshot.error);
+            }
+
+            switch (snapshot.connectionState) {
+              case ConnectionState.waiting:
+                return CircularProgressIndicator();
+                break;
+              default:
+                return snapshot.data.isNotEmpty
+                    ? GridView.builder(
+                        itemBuilder: (context, index) {
+                          return ProductCardComponent(snapshot.data[index]);
+                        },
+                        physics: BouncingScrollPhysics(),
+                        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 2,
+                          childAspectRatio: 1.5,
+                          crossAxisSpacing: SizeConfig.blockSizeVertical * 1.5,
+                          mainAxisSpacing: SizeConfig.blockSizeVertical * 1.5,
+                        ),
+                        itemCount: snapshot.data.length,
+                        padding:
+                            EdgeInsets.all(SizeConfig.blockSizeVertical * 1.5),
+                      )
+                    : Container(
+                        width: SizeConfig.blockSizeHorizontal * 100,
+                        height: SizeConfig.blockSizeVertical * 100,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Image.asset('assets/shop.png'),
+                            onlyFavorites
+                                ? Text(
+                                    'Lista de favoritos vazia',
+                                    style: TextStyle(fontSize: 16),
+                                  )
+                                : Text(
+                                    'Lista de produtos vazia',
+                                    style: TextStyle(fontSize: 16),
+                                  ),
+                          ],
+                        ));
+            }
+          },
+        ));
   }
 }
--- lib/pages/registration_pages/products_list_page.dart
+++ lib/pages/registration_pages/products_list_page.dart
@@ -23,117 +23,115 @@ class _ProductsListPageState extends State<ProductsListPage> {
 
   @override
   Widget build(BuildContext context) {
-    final ProductsViewModel productsViewModel =
-        Provider.of<ProductsViewModel>(context);
     final FavoriteViewModel favoriteViewModel =
         Provider.of<FavoriteViewModel>(context, listen: false);
 
-    return StreamBuilder<List<ProductModel>>(
-        stream: _productsStream,
-        builder: (context, snapshot) {
-          if (snapshot.hasError) {
-            print(snapshot.data);
-            return Text('Error: ${snapshot.error}');
-          }
-          switch (snapshot.connectionState) {
-            case ConnectionState.waiting:
-              return Text('Loading');
-            default:
-              return Scaffold(
-                appBar: AppBar(
-                  title: Text(
-                    'Produtos',
-                    style: TextStyle(color: Colors.black54),
-                  ),
-                  elevation: 0,
-                  backgroundColor: Colors.white12,
-                  iconTheme: IconThemeData(color: Colors.black54),
-                  actions: [
-                    IconButton(
-                        icon: Icon(Icons.add),
-                        color: Theme.of(context).primaryColor,
-                        onPressed: () =>
-                            Navigator.pushNamed(context, 'product_form'))
-                  ],
-                ),
-                drawer: DrawerView(),
-                body: ListView.builder(
-                  physics: BouncingScrollPhysics(),
-                  itemCount: snapshot.data?.length,
-                  itemBuilder: (context, index) {
-                    final ProductModel product = snapshot?.data[index];
-                    return Card(
-                      elevation: 2,
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(24))),
-                      child: ListTile(
-                        title: Text(product.name),
-                        leading: ClipOval(
-                          child: Container(
-                              child: FadeInImage.memoryNetwork(
-                            placeholder: kTransparentImage,
-                            image: product.imageUrl,
-                            fit: BoxFit.cover,
-                            height: SizeConfig.blockSizeHorizontal * 12,
-                            width: SizeConfig.blockSizeHorizontal * 12,
-                          )),
-                        ),
-                        trailing: Row(
-                          mainAxisAlignment: MainAxisAlignment.end,
-                          mainAxisSize: MainAxisSize.min,
-                          children: [
-                            IconButton(
-                                icon: Icon(Icons.edit),
-                                color: Theme.of(context).primaryColor,
-                                onPressed: () {
-                                  Navigator.of(context).pushNamed(
-                                      'product_form',
-                                      arguments: product);
-                                }),
-                            IconButton(
-                                icon: Icon(Icons.delete),
-                                color: Colors.redAccent,
-                                onPressed: () {
-                                  showDialog(
-                                    context: context,
-                                    builder: (context) {
-                                      return AlertDialog(
-                                        title: Text('Excluir produto'),
-                                        content: Text(
-                                            'Você deseja excluir o produto ${product.name}?'),
-                                        actions: [
-                                          FlatButton(
-                                            onPressed: () =>
-                                                Navigator.of(context).pop(),
-                                            child: Text('Cancelar'),
-                                            textColor: Colors.grey,
-                                          ),
-                                          FlatButton(
-                                            onPressed: () {
-                                              if (favoriteViewModel.favorites
-                                                  .contains(product)) {
-                                                favoriteViewModel
-                                                    .toggleFavorite(product);
-                                              }
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'Produtos',
+            style: TextStyle(color: Colors.black54),
+          ),
+          elevation: 0,
+          backgroundColor: Colors.white12,
+          iconTheme: IconThemeData(color: Colors.black54),
+          actions: [
+            IconButton(
+                icon: Icon(Icons.add),
+                color: Theme.of(context).primaryColor,
+                onPressed: () => Navigator.pushNamed(context, 'product_form'))
+          ],
+        ),
+        drawer: DrawerView(),
+        body: StreamBuilder<List<ProductModel>>(
+            stream: _productsStream,
+            builder: (context, snapshot) {
+              if (snapshot.hasError) {
+                print(snapshot.data);
+                return Text('Error: ${snapshot.error}');
+              }
+              switch (snapshot.connectionState) {
+                case ConnectionState.waiting:
+                  return CircularProgressIndicator();
+                default:
+                  return ListView.builder(
+                    physics: BouncingScrollPhysics(),
+                    itemCount: snapshot.data?.length,
+                    itemBuilder: (context, index) {
+                      final ProductModel product = snapshot?.data[index];
+                      return Card(
+                        elevation: 2,
+                        shape: RoundedRectangleBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(24))),
+                        child: ListTile(
+                          title: Text(product.name),
+                          leading: ClipOval(
+                            child: Container(
+                                child: FadeInImage.memoryNetwork(
+                              placeholder: kTransparentImage,
+                              image: product.imageUrl,
+                              fit: BoxFit.cover,
+                              height: SizeConfig.blockSizeHorizontal * 12,
+                              width: SizeConfig.blockSizeHorizontal * 12,
+                            )),
+                          ),
+                          trailing: Row(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              IconButton(
+                                  icon: Icon(Icons.edit),
+                                  color: Theme.of(context).primaryColor,
+                                  onPressed: () {
+                                    Navigator.of(context).pushNamed(
+                                        'product_form',
+                                        arguments: product);
+                                  }),
+                              IconButton(
+                                  icon: Icon(Icons.delete),
+                                  color: Colors.redAccent,
+                                  onPressed: () {
+                                    showDialog(
+                                      context: context,
+                                      builder: (context) {
+                                        return AlertDialog(
+                                          title: Text('Excluir produto'),
+                                          content: Text(
+                                              'Você deseja excluir o produto ${product.name}?'),
+                                          actions: [
+                                            FlatButton(
+                                              onPressed: () =>
+                                                  Navigator.of(context).pop(),
+                                              child: Text('Cancelar'),
+                                              textColor: Colors.grey,
+                                            ),
+                                            FlatButton(
+                                              onPressed: () {
+                                                if (favoriteViewModel.favorites
+                                                    .contains(product)) {
+                                                  favoriteViewModel
+                                                      .toggleFavorite(product);
+                                                }
 
-                                              // productsViewModel.removeProduct(product);
-                                              // Navigator.of(context).pop();
-                                            },
-                                            child: Text('Excluir'),
-                                          )
-                                        ],
-                                      );
-                                    },
-                                  );
-                                })
-                          ],
+                                                // productsViewModel.removeProduct(product);
+                                                // Navigator.of(context).pop();
+                                              },
+                                              child: Text('Excluir'),
+                                            )
+                                          ],
+                                        );
+                                      },
+                                    );
+                                  })
+                            ],
+                          ),
                         ),
-                      ),
-                    );
-                  },
-                ),
-              );
-          }
-        });
+                      );
+                    },
+                  );
+              }
+            }));
+    ;
   }
 }
