1eb1ed99abbd1a0798bad7d8505a88d8e44081e5
Léo Kiszewski <kiszewski1999@gmail.com>
2020-12-24 10:37:28 -0300

    refactoring order page and order card component

--- lib/pages/orders_page/orders_page.dart
+++ lib/pages/orders_page/orders_page.dart
@@ -1,54 +1,61 @@
-// import 'package:flutter/material.dart';
-// import 'package:provider/provider.dart';
-// import 'package:shopApp/pages/components/drawer/drawer_view.dart';
-// import 'package:shopApp/utils/size_config.dart';
-// import 'package:shopApp/viewmodels/orders_viewmodel.dart';
-// import 'package:shopApp/pages/orders_page/order_card_component.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shopApp/models/order_model.dart';
+import 'package:shopApp/pages/components/drawer/drawer_view.dart';
+import 'package:shopApp/repository/user_repository.dart';
+import 'package:shopApp/utils/size_config.dart';
+import 'package:shopApp/pages/orders_page/order_card_component.dart';
 
-// class OrdersPage extends StatefulWidget {
-//   @override
-//   _OrdersPageState createState() => _OrdersPageState();
-// }
+class OrdersPage extends StatefulWidget {
+  @override
+  _OrdersPageState createState() => _OrdersPageState();
+}
 
-// class _OrdersPageState extends State<OrdersPage> {
-//   @override
-//   Widget build(BuildContext context) {
-//     OrdersViewModel ordersModel = Provider.of<OrdersViewModel>(context);
+class _OrdersPageState extends State<OrdersPage> {
+  @override
+  Widget build(BuildContext context) {
+    UserRepository userRepository = Provider.of<UserRepository>(context);
 
-//     return Scaffold(
-//       appBar: AppBar(
-//         iconTheme: IconThemeData(color: Colors.black87),
-//         elevation: 0,
-//         backgroundColor: Colors.white12,
-//         title: Text(
-//           'Suas compras',
-//           style: TextStyle(color: Colors.black87),
-//         ),
-//       ),
-//       drawer: DrawerView(),
-//       body: ordersModel.orders.isEmpty
-//           ? Container(
-//               width: SizeConfig.blockSizeHorizontal * 100,
-//               height: SizeConfig.blockSizeVertical * 100,
-//               padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-//               child: Column(
-//                 mainAxisAlignment: MainAxisAlignment.center,
-//                 children: [
-//                   Image.asset('assets/shop.png'),
-//                   Text(
-//                     'Sem compras registradas',
-//                     style: TextStyle(fontSize: 16),
-//                   ),
-//                 ],
-//               ),
-//             )
-//           : ListView.builder(
-//               physics: BouncingScrollPhysics(),
-//               itemBuilder: (context, index) {
-//                 return OrderCard(ordersModel.orders[index]);
-//               },
-//               itemCount: ordersModel.orders.length,
-//             ),
-//     );
-//   }
-// }
+    return Scaffold(
+      appBar: AppBar(
+        iconTheme: IconThemeData(color: Colors.black87),
+        elevation: 0,
+        backgroundColor: Colors.white12,
+        title: Text(
+          'Suas compras',
+          style: TextStyle(color: Colors.black87),
+        ),
+      ),
+      drawer: DrawerView(),
+      body: StreamBuilder<List<OrderModel>>(
+        stream: userRepository.getOrders(),
+        initialData: [],
+        builder: (context, snapshot) {
+          return snapshot.data.isEmpty
+              ? Container(
+                  width: SizeConfig.blockSizeHorizontal * 100,
+                  height: SizeConfig.blockSizeVertical * 100,
+                  padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image.asset('assets/shop.png'),
+                      Text(
+                        'Sem compras registradas',
+                        style: TextStyle(fontSize: 16),
+                      ),
+                    ],
+                  ),
+                )
+              : ListView.builder(
+                  physics: BouncingScrollPhysics(),
+                  itemCount: snapshot.data.length,
+                  itemBuilder: (context, index) {
+                    return OrderCard(snapshot.data[index]);
+                  },
+                );
+        },
+      ),
+    );
+  }
+}
